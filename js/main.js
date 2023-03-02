fd82f3fc5422bfeb868e5f1ed4073bd26cb7858b
Kamil <mr.kaam@gmail.com>
2023-03-02 23:42:55 +0100

    SIDEBAR: refactoring. Update class names and JS code

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
-const sidebar = document.querySelector('.sidebar');
-const sidebarDescriptions = document.querySelectorAll('.sidebar__description');
-const sidebarShadow = document.querySelector('.sidebar__shadow');
 const nav = document.querySelector('.nav');
 const app = document.querySelector('.app');
+const sidebar = document.querySelector('.sidebar');
+const sidebarDescriptions = document.querySelectorAll('.sidebar__description');
 const burgerBtn = document.querySelector('.nav__mobile-burger-btn');
 
 const openSideBar = () => {
-	sidebar.classList.remove('sidebar--short');
-	nav.classList.remove('nav--wide');
-	app.classList.remove('app--wide');
-
-	setTimeout(() => {
-		sidebarDescriptions.forEach((el) => {
-			el.classList.remove('sidebar__description--hide');
-		});
-	}, 500);
-};
-
-const hideSideBar = () => {
-	sidebar.classList.add('sidebar--short');
+	sidebar.classList.add('sidebar--open');
 	nav.classList.add('nav--wide');
 	app.classList.add('app--wide');
 	sidebarDescriptions.forEach((el) => {
-		el.classList.add('sidebar__description--hide');
+		el.classList.add('sidebar__description--visible');
+	});
+};
+
+const hideSideBar = () => {
+	sidebar.classList.remove('sidebar--open');
+	nav.classList.remove('nav--wide');
+	app.classList.remove('app--wide');
+
+	sidebarDescriptions.forEach((el) => {
+		el.classList.remove('sidebar__description--visible');
 	});
 };
 
 const toggleSidebar = () => {
-	if (!sidebar.classList.contains('sidebar--short')) {
-		hideSideBar();
-	} else {
+	if (!sidebar.classList.contains('sidebar--open')) {
 		openSideBar();
+	} else {
+		hideSideBar();
 	}
 };
 
-sidebar.addEventListener('click', toggleSidebar);
+sidebar.addEventListener('mousemove', openSideBar);
+sidebar.addEventListener('mouseleave', hideSideBar);
 burgerBtn.addEventListener('click', toggleSidebar);
