99e01952f70b89ba29246339566ce61ce2a984bd
Kamil <mr.kaam@gmail.com>
2023-03-06 11:20:16 +0100

    change js to fix bug in google chrome

--- js/main.js
+++ js/main.js
@@ -107,9 +107,8 @@ const startEditText = (e) => {
 	const closestEditBtn = e.target.closest('div').childNodes[1];
 	const closestSaveBtn = e.target.closest('div').childNodes[2];
 
-	// closestItemText.setAttribute('contenteditable', 'true');
-	closestItemText.contentEditable = 'true';
 	closestItemText.classList.add('app__tasks-item-text--edit');
+	closestItemText.contentEditable = 'true';
 	closestItemText.focus();
 	closestDoneBtn.style.visibility = 'hidden';
 	closestDeleteBtn.style.visibility = 'hidden';
@@ -125,9 +124,8 @@ const saveEditedText = (e) => {
 	const closestEditBtn = e.target.closest('div').childNodes[1];
 	const closestSaveBtn = e.target.closest('div').childNodes[2];
 
-	// closestItemText.setAttribute('contenteditable', 'false');
-	closestItemText.contentEditable = 'false';
 	closestItemText.classList.remove('app__tasks-item-text--edit');
+	closestItemText.contentEditable = 'false';
 	closestDoneBtn.style.visibility = 'visible';
 	closestDeleteBtn.style.visibility = 'visible';
 	closestEditBtn.style.display = 'block';
