732d23adda7ece857dcf3ba05a7cfb26383d782c
Kamil <mr.kaam@gmail.com>
2023-03-16 13:37:51 +0100

    REFACTORING: extract nav feature to external .js file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 import './sidebar.js';
+import './nav.js'
 import './toDoApp.js';
 import './settings.js';
 
-const navAvatar = document.querySelector('.nav__avatar');
-const navMenu = document.querySelector('.nav__menu');
-const navMenuList = document.querySelector('.nav__menu-settings');
 const navMenuItems = document.querySelectorAll('.nav__menu-settings-item');
 
 const app = document.querySelector('.app');
@@ -13,20 +11,11 @@ const sidebarItems = document.querySelectorAll('.sidebar__option');
 const sidebarSettingsItem = sidebarItems[1];
 const settings = document.querySelector('.settings');
 
-const slideNavMenu = () => {
-	navMenu.classList.toggle('nav__menu--visible');
-	navMenuList.classList.toggle('nav__menu-settings--slided');
-};
-
 const displaySettings = () => {
 	app.style.display = 'none';
 	settings.style.display = 'flex';
 };
-navAvatar.addEventListener('click', slideNavMenu);
 navMenuItems.forEach((item) => {
 	item.addEventListener('click', displaySettings);
-});
-navMenuItems.forEach((item) => {
-	item.addEventListener('click', slideNavMenu);
 });
 sidebarSettingsItem.addEventListener('click', displaySettings);
